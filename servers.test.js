734f21f178c5e6550e97ccc2f54d82215184320a
alocke8181 <alocke_8181@yahoo.com>
2023-03-29 19:03:49 -0400

    added more tests to servers.test

--- servers.test.js
+++ servers.test.js
@@ -11,7 +11,26 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it("should not add servers when there is no name for submitServerInfo()",function(){
+    serverNameInput.value = "";
+    submitServerInfo();
+    expect(Object.keys(allServers).length).toEqual('0');
+  });
+
+  it('should update the server table when updateServerTable() is called', function(){
+    submitServerInfo();
+    updateServerTable();
+    let testList = document.querySelectorAll('#serverTable tbody tr td');
+    expect(testList.length).toEqual('3');
+    expect(testList[0].innerText).toEqual('Alice');
+    expect(testList[1].innerText).toEqual('$0.00');
+    expect(testList[2].innerText).toEqual('X');
+  });
+
   afterEach(function() {
     // teardown logic
+    serverId = 0;
+    serverTbody.innerHTML = "";
+    allServers = {};
   });
 });
